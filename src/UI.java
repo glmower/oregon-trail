da146bd6bf684561661d5f8b74866dbff771e924
Grace Mower <gracemower04@gmail.com>
2025-08-21 13:05:03 -0700

    Added a UI window with a title and logo.

--- src/UI.java
+++ src/UI.java
@@ -1,3 +1,18 @@
-public class UI {
-    
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame; 
+
+public class UI extends JFrame {
+    public UI() {
+        this.setTitle("Aii's Oregon Trail");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800,600);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setResizable(false);
+
+        ImageIcon logo = new ImageIcon("images/Aii-logo.jpg");
+        this.setIconImage(logo.getImage());
+        // frame.getContentPane().setBackground(Color.BLACK);
+    }
 }
